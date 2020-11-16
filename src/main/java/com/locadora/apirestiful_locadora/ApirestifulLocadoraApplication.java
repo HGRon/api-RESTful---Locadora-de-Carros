9e971b114f21a667b91c3755faeac0402182a151
HGRon <higordasilvalins@gmail.com>
2020-11-16 19:55:10 -0300

    primero commit

---
+++ src/main/java/com/locadora/apirestiful_locadora/ApirestifulLocadoraApplication.java
@@ -0,0 +1,13 @@
+package com.locadora.apirestiful_locadora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestifulLocadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestifulLocadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locadora/apirestiful_locadora/ApirestifulLocadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locadora.apirestiful_locadora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestifulLocadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
